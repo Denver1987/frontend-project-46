f98d56f83ee577b1fa5fcc3fe87974f9a0822c7e
Denver1987 <cm-den@yandex.ru>
2024-06-01 23:18:35 +0300

    Доработка

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -16,10 +16,6 @@ program.argument('<filepath1>')
   .argument('<filepath2>');
 
 program.action((filepath1, filepath2) => {
-  console.log(path.dirname(path.resolve(filepath1)));
-  console.log(path.basename(path.resolve(filepath1)));
-  console.log(path.extname(path.resolve(filepath1)));
-
   if (path.extname(path.resolve(filepath1)) === '.json' && path.extname(path.resolve(filepath2)) === '.json') {
     console.log(showDifference(filepath1, filepath2));
   }
--- src/gendiff.js
+++ src/gendiff.js
@@ -5,17 +5,20 @@ function parseJSONFile(filepath) {
   return JSON.parse(readFileSync(filepath, 'utf8'));
 }
 
+function reduce(previous, key, obj1, obj2) {
+  if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) return `${previous}\n  - ${key}: ${obj1[key]}`;
+  if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) return `${previous}\n  + ${key}: ${obj2[key]}`;
+  if (obj1[key] === obj2[key]) return `${previous}\n    ${key}: ${obj1[key]}`;
+  if (obj1[key] !== obj2[key]) return `${previous}\n  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+}
+
 export default function showDifference(file1, file2) {
   const obj1 = parseJSONFile(file1);
   const obj2 = parseJSONFile(file2);
   let keys = _.union(Object.keys(obj1), Object.keys(obj2));
   keys = _.sortBy(keys);
-  // eslint-disable-next-line array-callback-return, consistent-return
   const result = keys.reduce((previous, key) => {
-    if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) return `${previous}\n  - ${key}: ${obj1[key]}`;
-    if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) return `${previous}\n  + ${key}: ${obj2[key]}`;
-    if (obj1[key] === obj2[key]) return `${previous}\n    ${key}: ${obj1[key]}`;
-    if (obj1[key] !== obj2[key]) return `${previous}\n  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+    return reduce(previous, key, obj1, obj2);
   }, '');
   return `{${result}\n}\n`;
 }
