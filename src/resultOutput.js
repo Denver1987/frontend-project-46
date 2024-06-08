9b280441166a91f617ff8be5778567b6d7d6f701
Denver1987 <cm-den@yandex.ru>
2024-06-09 00:08:20 +0300

    Доработка форматтера

--- src/resultOutput.js
+++ src/resultOutput.js
@@ -25,27 +25,32 @@ export function formStylish(changings, obj1, obj2, level) {
       result += `\n${indent.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], nextLevel)}`;
     } else result += `\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
   }
+
   function printDelete(key) {
     if (isObject(obj1[key])) {
       result += `\n${indent.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], nextLevel)}`;
     } else result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}`;
   }
 
+  function printChanged(key) {
+    printDelete(key);
+    printAdded(key);
+  }
+
+  function printUnchanged(key) {
+    result += `\n${indent.repeat(level - 1)}    ${key}: ${obj1[key]}`;
+  }
+
+  function printInnerChangedObject(key) {
+    result += `\n${indent.repeat(level - 1)}    ${key}: ${formStylish(differenceObjects(obj1[key], obj2[key]), obj1[key], obj2[key], nextLevel)}`;
+  }
+
   keys.forEach((key) => {
-    if (isObject(obj1[key]) && isObject(obj2[key])) {
-      result += `\n${indent.repeat(level - 1)}    ${key}: ${formStylish(differenceObjects(obj1[key], obj2[key]), obj1[key], obj2[key], nextLevel)}`;
-    } else if (changings[key] === 'changed') {
-      if (isObject(obj1[key]) && !isObject(obj2[key])) {
-        result += `\n${indent.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], nextLevel)}\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
-      } else if (!isObject(obj1[key]) && isObject(obj2[key])) {
-        result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}\n${indent.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], nextLevel)}`;
-      } else result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
-    } else if (changings[key] === 'unchanged') result += `\n${indent.repeat(level - 1)}    ${key}: ${obj1[key]}`;
-    else if (changings[key] === 'added') {
-      printAdded(key);
-    } else if (changings[key] === 'deleted') {
-      printDelete(key);
-    }
+    if (isObject(obj1[key]) && isObject(obj2[key])) printInnerChangedObject(key);
+    else if (changings[key] === 'changed') printChanged(key);
+    else if (changings[key] === 'unchanged') printUnchanged(key);
+    else if (changings[key] === 'added') printAdded(key);
+    else if (changings[key] === 'deleted') printDelete(key);
   });
   return `{${result}\n${indent.repeat(level - 1)}}`;
 }
