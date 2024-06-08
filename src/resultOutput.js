49e57e77939d483af3a1a9569766a36aa9b409ff
Denver1987 <cm-den@yandex.ru>
2024-06-09 02:19:09 +0300

    Доработка модуля вывода

--- src/resultOutput.js
+++ src/resultOutput.js
@@ -8,8 +8,7 @@ export function printObject(obj, level) {
   let result = '{';
   keys.forEach((key) => {
     if (isObject(obj[key])) {
-      const nextLevel = level + 1;
-      result += `\n${INDENT.repeat(level - 1)}    ${key}: ${printObject(obj[key], nextLevel)}`;
+      result += `\n${INDENT.repeat(level - 1)}    ${key}: ${printObject(obj[key], level + 1)}`;
     } else result += `\n${INDENT.repeat(level - 1)}    ${key}: ${obj[key]}`;
   });
   return `${result}\n${INDENT.repeat(level - 1)}}`;
@@ -19,39 +18,38 @@ function printUnchanged(key, obj1, level) {
   return `\n${INDENT.repeat(level - 1)}    ${key}: ${obj1[key]}`;
 }
 
-function printAdded(key, obj2, level, nextLevel) {
+function printAdded(key, obj2, level) {
   if (isObject(obj2[key])) {
-    return `\n${INDENT.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], nextLevel)}`;
+    return `\n${INDENT.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], level + 1)}`;
   } return `\n${INDENT.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
 }
 
-function printDelete(key, obj1, level, nextLevel) {
+function printDelete(key, obj1, level) {
   if (isObject(obj1[key])) {
-    return `\n${INDENT.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], nextLevel)}`;
+    return `\n${INDENT.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], level + 1)}`;
   } return `\n${INDENT.repeat(level - 1)}  - ${key}: ${obj1[key]}`;
 }
 
-function printChanged(key, obj1, obj2, level, nextLevel) {
-  return `${printDelete(key, obj1, level, nextLevel)}${printAdded(key, obj2, level, nextLevel)}`;
+function printChanged(key, obj1, obj2, level) {
+  return `${printDelete(key, obj1, level)}${printAdded(key, obj2, level)}`;
 }
 
-function printInnerChangedObject(key, obj1, obj2, level, nextLevel) {
+function printInnerChangedObject(key, obj1, obj2, level) {
   // eslint-disable-next-line no-use-before-define
-  return `\n${INDENT.repeat(level - 1)}    ${key}: ${formStylish(differenceObjects(obj1[key], obj2[key]), obj1[key], obj2[key], nextLevel)}`;
+  return `\n${INDENT.repeat(level - 1)}    ${key}: ${formStylish(differenceObjects(obj1[key], obj2[key]), obj1[key], obj2[key], level + 1)}`;
 }
 
-export function formStylish(changings, obj1, obj2, level) {
+export function formStylish(changings, obj1, obj2, level = 1) {
   const keys = _.sortBy(Object.keys(changings));
   let result = '';
-  const nextLevel = level + 1;
 
   keys.forEach((key) => {
     if (isObject(obj1[key]) && isObject(obj2[key])) {
-      result += printInnerChangedObject(key, obj1, obj2, level, nextLevel);
-    } else if (changings[key] === 'changed') result += printChanged(key, obj1, obj2, level, nextLevel);
+      result += printInnerChangedObject(key, obj1, obj2, level);
+    } else if (changings[key] === 'changed') result += printChanged(key, obj1, obj2, level);
     else if (changings[key] === 'unchanged') result += printUnchanged(key, obj1, level);
-    else if (changings[key] === 'added') result += printAdded(key, obj2, level, nextLevel);
-    else if (changings[key] === 'deleted') result += printDelete(key, obj1, level, nextLevel);
+    else if (changings[key] === 'added') result += printAdded(key, obj2, level);
+    else if (changings[key] === 'deleted') result += printDelete(key, obj1, level);
   });
   return `{${result}\n${INDENT.repeat(level - 1)}}`;
 }
