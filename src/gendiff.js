0d19c45dbec83c17932f3b44d3a2d9401f976e45
Denver1987 <cm-den@yandex.ru>
2024-06-08 19:25:01 +0300

    Реализовано сравнение сложных JSON-файлов

--- src/gendiff.js
+++ src/gendiff.js
@@ -6,13 +6,17 @@ function isObject(variable) {
   return Object.prototype.toString.call(variable) === '[object Object]';
 }
 
-// eslint-disable-next-line consistent-return
-function formatStylish(previous, key, obj1, obj2, level) {
-  const indent = '  ';
-  if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) return `${previous}\n${indent.repeat(level)}- ${key}: ${obj1[key]}`;
-  if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) return `${previous}\n${indent.repeat(level)}+ ${key}: ${obj2[key]}`;
-  if (obj1[key] === obj2[key]) return `${previous}\n${indent.repeat(level)}  ${key}: ${obj1[key]}`;
-  if (obj1[key] !== obj2[key]) return `${previous}\n${indent.repeat(level)}- ${key}: ${obj1[key]}\n${indent.repeat(level)}+ ${key}: ${obj2[key]}`;
+function printObject(obj, level) {
+  const keys = Object.keys(obj);
+  const indent = '    ';
+  let result = '{';
+  keys.forEach((key) => {
+    if (isObject(obj[key])) {
+      const nextLevel = level + 1;
+      result += `\n${indent.repeat(level - 1)}    ${key}: ${printObject(obj[key], nextLevel)}`;
+    } else result += `\n${indent.repeat(level - 1)}    ${key}: ${obj[key]}`;
+  });
+  return `${result}\n${indent.repeat(level - 1)}}`;
 }
 
 function differenceObjects(obj1, obj2) {
@@ -29,8 +33,40 @@ function differenceObjects(obj1, obj2) {
   return changings;
 }
 
+function formOutput(changings, obj1, obj2, level) {
+  const keys = _.sortBy(Object.keys(changings));
+  const indent = '    ';
+  let result = '';
+  const nextLevel = level + 1;
+  keys.forEach((key) => {
+    if (isObject(obj1[key]) && isObject(obj2[key])) {
+      result += `\n${indent.repeat(level - 1)}    ${key}: ${formOutput(differenceObjects(obj1[key], obj2[key]), obj1[key], obj2[key], nextLevel)}`;
+    } else if (changings[key] === 'changed') {
+      if (isObject(obj1[key]) && !isObject(obj2[key])) {
+        result += `\n${indent.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], nextLevel)}\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
+      } else if (!isObject(obj1[key]) && isObject(obj2[key])) {
+        result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}\n${indent.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], nextLevel)}`;
+      } else result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
+    } else if (changings[key] === 'unchanged') result += `\n${indent.repeat(level - 1)}    ${key}: ${obj1[key]}`;
+    else if (changings[key] === 'added') {
+      if (isObject(obj2[key])) {
+        result += `\n${indent.repeat(level - 1)}  + ${key}: ${printObject(obj2[key], nextLevel)}`;
+      } else result += `\n${indent.repeat(level - 1)}  + ${key}: ${obj2[key]}`;
+    } else if (changings[key] === 'deleted') {
+      if (isObject(obj1[key])) {
+        result += `\n${indent.repeat(level - 1)}  - ${key}: ${printObject(obj1[key], nextLevel)}`;
+      } else result += `\n${indent.repeat(level - 1)}  - ${key}: ${obj1[key]}`;
+    }
+  });
+  return `{${result}\n${indent.repeat(level - 1)}}`;
+}
+
 export default function showDifference(file1, file2) {
   const obj1 = parseFile(file1);
   const obj2 = parseFile(file2);
+
   const changings = differenceObjects(obj1, obj2);
+  // console.log(obj1, obj2);
+  // console.log(printObject(obj1, 1));
+  return `${formOutput(changings, obj1, obj2, 1)}\n`;
 }
