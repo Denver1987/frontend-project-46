684e9dfc93d41d11ef959fd4f091adee36a5c02e
Denver1987 <cm-den@yandex.ru>
2024-06-11 22:14:58 +0300

    Доработка

--- src/differencers.js
+++ src/differencers.js
@@ -6,35 +6,37 @@ export function isObject(variable) {
 
 export function differenceObjects(obj1, obj2) {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const changings = {};
+  const result = {};
   keys.forEach((key) => {
     if (isObject(obj1[key]) && isObject(obj2[key])) {
-      changings[key] = {
-        status: 'nested',
-        value: differenceObjects(obj1[key], obj2[key]),
-      };
+      Object.assign(result, {
+        [key]: {
+          status: 'nested',
+          value: differenceObjects(obj1[key], obj2[key]),
+        },
+      });
     } else if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      changings[key] = {
+      result[key] = {
         status: 'deleted',
         oldValue: obj1[key],
       };
     } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      changings[key] = {
+      result[key] = {
         status: 'added',
         newValue: obj2[key],
       };
     } else if (obj1[key] === obj2[key]) {
-      changings[key] = {
+      result[key] = {
         status: 'unchanged',
         value: obj2[key],
       };
     } else if (obj1[key] !== obj2[key]) {
-      changings[key] = {
+      result[key] = {
         status: 'changed',
         oldValue: obj1[key],
         newValue: obj2[key],
       };
     }
   });
-  return changings;
+  return result;
 }
