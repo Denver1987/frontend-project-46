12d7ef49efdafaaafbf56d77be3868b525b11667
Denver1987 <cm-den@yandex.ru>
2024-06-15 03:01:02 +0300

    differencer refactoring

--- src/differencers.js
+++ src/differencers.js
@@ -4,25 +4,29 @@ import { isObject } from './utils.js';
 // eslint-disable-next-line consistent-return
 function makeChanging(key, obj1, obj2) {
   if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-    return [key, {
+    return {
+      key,
       status: 'deleted',
       value: obj1[key],
-    }];
+    };
   } if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-    return [key, {
+    return {
+      key,
       status: 'added',
       value: obj2[key],
-    }];
+    };
   } if (obj1[key] === obj2[key]) {
-    return [key, {
+    return {
+      key,
       status: 'unchanged',
       value: obj2[key],
-    }];
+    };
   } if (obj1[key] !== obj2[key]) {
-    return [key, {
+    return {
+      key,
       status: 'changed',
       value: [obj1[key], obj2[key]],
-    }];
+    };
   }
 }
 
@@ -32,11 +36,11 @@ export function differenceObjects(obj1, obj2) {
 
   return sortedKeys.map((key) => {
     if (isObject(obj1[key]) && isObject(obj2[key])) {
-      return [key,
-        {
-          status: 'nested',
-          child: differenceObjects(obj1[key], obj2[key]),
-        }];
+      return {
+        key,
+        status: 'nested',
+        child: differenceObjects(obj1[key], obj2[key]),
+      };
     }
 
     return makeChanging(key, obj1, obj2);
@@ -44,11 +48,11 @@ export function differenceObjects(obj1, obj2) {
 }
 
 export function getStatus(changing) {
-  return changing[1].status;
+  return changing.status;
 }
 
 export function getKeyName(changing) {
-  return changing[0];
+  return changing.key;
 }
 
 export function getValue(changing) {
@@ -56,27 +60,11 @@ export function getValue(changing) {
     case 'added':
     case 'deleted':
     case 'unchanged':
-    case 'changed': return changing[1].value;
+    case 'changed': return changing.value;
     default: throw new Error('Нераспознанный статус изменения или узел содержит вложенный объект.');
   }
 }
 
-export function getOldValue(chenging) {
-  return chenging[1].oldValue;
-}
-
-export function getNewValue(chenging) {
-  return chenging[1].newValue;
-}
-
-export function getUnchangedValue(chenging) {
-  return chenging[1].value;
-}
-
 export function getInnerChangings(changing) {
-  return changing[1].child;
-}
-
-export function getChangingValues(changing) {
-  return [changing[1].oldValue, changing[1].newValue];
+  return changing.child;
 }
